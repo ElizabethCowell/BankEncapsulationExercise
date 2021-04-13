227cce2732479d879988aec939f5a755c364781d
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-13 14:37:46 -0400

    added atm loop

--- BankAccount.cs
+++ BankAccount.cs
@@ -9,16 +9,54 @@ namespace BankEncapsulationExercise
     public class BankAccount
     {
         private double balance = 0;
+        private string password = "password";
 
-        public double Deposit()
+        public bool Verification()
         {
-            Console.WriteLine("How much are you depositing today?");
-            var deposit = int.Parse(Console.ReadLine());
-            return balance += deposit;
+            Console.WriteLine("What is your password?");
+            var answer = Console.ReadLine();
+            if (answer == password)
+            {
+                return true;
+            }
+            else
+                Console.WriteLine("Incorrect Password");
+                return false;
+        }
+        public void Deposit()
+        {
+            var verify = Verification();
+            if (verify)
+            {
+                Console.WriteLine("How much are you depositing today?");
+                var deposit = int.Parse(Console.ReadLine());
+                balance += deposit;
+            }
         }
         public void GetBalance()
         {
-            Console.WriteLine(balance);
+            var verify = Verification();
+            if (verify)
+            {
+                Console.WriteLine(balance);
+            }
+        }
+        public void Withdraw()
+        {
+            var verify = Verification();
+            if (verify)
+            {
+                Console.WriteLine("How much would you like to withdraw?");
+                var withdraw = int.Parse(Console.ReadLine());
+                if (withdraw > balance)
+                {
+                    Console.WriteLine("Insuficiant funds");
+                }
+                else
+                {
+                    balance -= withdraw;
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,8 +7,33 @@ namespace BankEncapsulationExercise
         static void Main(string[] args)
         {
             var newBankAccount = new BankAccount();
-            newBankAccount.Deposit();
-            newBankAccount.GetBalance();
+            string selection;
+            do
+            {
+                Console.WriteLine("What would you like to do today? \n" +
+                                    "Deposit \n" +
+                                    "Withdraw \n" +
+                                    "Check balance \n" +
+                                    "Exit");
+                selection = Console.ReadLine().ToLower();
+                switch (selection)
+                {
+                    case ("deposit"):
+                        newBankAccount.Deposit();
+                        break;
+                    case ("withdraw"):
+                        newBankAccount.Withdraw();
+                        break;
+                    case ("check balance"):
+                        newBankAccount.GetBalance();
+                        break;
+                    case ("exit"):
+                        break;
+                    default:
+                        Console.WriteLine("Please try again");
+                        break;
+                }
+            } while (selection != "exit");
             
         }
     }
