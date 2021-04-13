a3ead65954adfba6c28e3beaedee6a26a0a65c2e
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-13 12:33:01 -0400

    first solution

---
+++ BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public double Deposit()
+        {
+            Console.WriteLine("How much are you depositing today?");
+            var deposit = int.Parse(Console.ReadLine());
+            return balance += deposit;
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine(balance);
+        }
+    }
+}
